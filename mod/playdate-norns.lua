d52b462e773e9891c981744711f832177bca6a13
Robin <midouesterner@gmail.com>
2025-02-23 15:02:04 -0600

    initial commit

---
+++ mod/playdate-norns.lua
@@ -0,0 +1,111 @@
+local mod = require('core/mods')
+local serial = require('core/serial')
+
+local config = {
+  ispeed = serial.B115200,
+  ospeed = serial.B115200,
+  cflag = serial.CS8 | serial.CLOCAL | serial.CREAD,
+  iflag = ~(serial.IXON | serial.IXOFF | serial.IXANY),
+  oflag = 0,
+  lflag = 0,
+  cc={
+    [serial.VMIN]=0,
+    [serial.VTIME]=5,
+  },
+}
+
+playdate = {}
+
+_norns.playdate = {}
+
+function _norns.playdate.init()
+  playdate.add = nil
+  playdate.remove = nil
+  playdate.event = nil
+  playdate.accel = nil
+  playdate.button = nil
+  playdate.crank = nil
+  playdate.crankdock = nil
+end
+
+function _norns.playdate.add(id, name, dev)
+  print("playdate add: "..id.." "..name)
+  serial.send(dev, "echo off\n")
+end
+
+function _norns.playdate.remove(id)
+  print("playdate remove: "..id)
+end
+
+local ebuffer = ""
+function _norns.playdate.event(id, line)
+  local function evalplaydate(line)
+    line = ebuffer..line
+    ebuffer = ""
+    if line == "echo off" then
+      return
+    elseif line:sub(1, 6) == "~ctl: " then
+      if line:sub(7, 12) == "accel " then
+        if playdate.accel ~= nil then
+          local next = line:sub(13, #line):gmatch("%S+")
+          local x = tonumber(next())
+          local y = tonumber(next())
+          local z = tonumber(next())
+          playdate.accel(x, y, z)
+        end
+      elseif line:sub(7, 12) == "crank " then
+        if playdate.crank ~= nil then
+          local d = tonumber(line:sub(13, #line))
+          playdate.crank(d)
+        end
+      elseif line:sub(7, 10) == "btn " then
+        if playdate.button ~= nil then
+          local b = line:sub(11, 11)
+          local s = tonumber(line:sub(13, 13))
+          playdate.button(b, s)
+        end
+      elseif line:sub(7, 16) == "crankdock " then
+        if playdate.crankdock ~= nil then
+          local s = tonumber(line:sub(17, 17))
+          playdate.crankdock(s)
+        end
+      end
+    else
+      if playdate.event ~= nil then
+          playdate.event(line)
+      end
+    end
+  end
+  
+  local n, m = line:find("%c+")
+  if not n then
+    ebuffer = ebuffer..line
+  else
+    evalplaydate(line:sub(1, n-1))
+    if m < #line then
+      _norns.playdate.event(id, line:sub(m+1, -1))
+    end
+  end
+end
+
+mod.hook.register("system_startup", "playdate device", function()
+  serial.handler{
+    id="playdate",
+    configure=function(vendor, model)
+      if vendor == "Panic_Inc" and model == "Playdate" then
+        return config
+      end
+    end,
+    add=_norns.playdate.add,
+    remove=_norns.playdate.remove,
+    event=_norns.playdate.event,
+  }
+end)
+
+mod.hook.register("script_pre_init", "init playdate device handlers", function()
+  _norns.playdate.init()
+end)
+
+mod.hook.register("script_post_cleanup", "init playdate device handlers", function()
+  _norns.playdate.init()
+end)
